5b871dcb7566c144ea885b7d54636d320325f1d9
igorjv123 <33521127+igorjv123@users.noreply.github.com>
2018-07-19 21:57:27 +0300

    Update improvedFighter.ts

--- src/public/fight/improvedFighter.ts
+++ src/public/fight/improvedFighter.ts
@@ -2,10 +2,9 @@
 // ImprovedFighter inherits propertіes from "Fighter"
 // And has new method 'doubleHit', that makes 2 x hit
 
-// Виконано побажання з конструктором
 import Fighter from "./fighter";
 class ImprovedFighter extends Fighter {
-  // doubleHit виправлено
+ 
   doubleHit(enemy, point) {
     super.hit(enemy, point * 2);
   }
